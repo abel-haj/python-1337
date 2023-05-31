dee60190a8748435ab12efd731bf3d3ece2f5236
Abdelaziz BELHAJ <abdelaziz@Abdelazizs-iMac.local>
2023-05-31 17:24:11 +0100

    removed space at the end

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -1,5 +1,4 @@
 import sys
 
 if len(sys.argv) > 1:
-    for i in range(1, len(sys.argv)):
-        print(sys.argv[len(sys.argv)-i].swapcase()[::-1], end=" ")
+    print(' '.join(sys.argv[1:])[::-1].swapcase())
