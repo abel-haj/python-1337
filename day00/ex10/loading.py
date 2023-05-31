67199e040b057db45b43d0b9e7922b5eabb343db
Abdelaziz BELHAJ <abdelaziz@Abdelazizs-iMac.local>
2023-05-31 17:16:12 +0100

    finished progress bar function ex10

--- day00/ex10/loading.py
+++ day00/ex10/loading.py
@@ -1,7 +1,44 @@
+import time
+import sys
 
 def ft_progress(lst):
-    pass
+    start = time.time()
+    for i in lst:
+        yield i
+
+        percent = (i + 1) / len(lst)
+        progress = "=" * int(percent * 19)
+        ela = time.time() - start
+        eta = 0 if i == 0 else ela * (len(lst) - i) / i
+
+        print(
+            "ETA: {:6.2f}s [{:3d}%] [{:<20s}] {:d}/{:d} elapsed time {:6.2f}s"
+            .format(
+                eta,
+                int(percent * 100),
+                progress+">",
+                i + 1,
+                len(lst),
+                ela
+            ),
+            end="\r",
+        )
 
 
-if __name__ == "__main__":
-    pass
+listy = range(1000)
+ret = 0
+for elem in ft_progress(listy):
+    ret += (elem + 3) % 5
+    time.sleep(0.01)
+print()
+print(ret)
+
+print()
+
+listy = range(3333)
+ret = 0
+for elem in ft_progress(listy):
+    ret += elem
+    time.sleep(0.005)
+print()
+print(ret)
